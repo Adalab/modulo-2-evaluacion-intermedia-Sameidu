14796d3288dac1b24d1131de20c7bfa7aff47ea7
Sara Meixoeiro <sameidudu@gmail.com>
2022-10-31 10:59:02 +0100

    js functions

--- js/main.js
+++ js/main.js
@@ -4,21 +4,43 @@ const select = document.querySelector('.js-select');
 const button = document.querySelector('.js-button');
 const result = document.querySelector('.js-result');
 
-function getRandomNumber(max) {
+function randomNumber(max) {
     return Math.ceil(Math.random() * max);
    };
 
+function shadowRace(randomNumber) {
+    let mordor = 1;
+    if (randomNumber(5) === 1 || randomNumber(5) === 2 || randomNumber(5) === 3){
+        mordor = 2;
+    }
+    else if ( randomNumber(5) === 4) {
+        mordor = 3;
+    }
+    else if (randomNumber(5) === 5) {
+        mordor = 5;
+    }
+    return mordor;
+}
+
 function player() {
-    const playerRace = parseInt(result.value);
+    const playerRace = parseInt(select.value);
     return playerRace;
 }
 
-function war() {
-    if (playerRace > random) {
-        console.log (`Gana la alianza`);
-    } else if (playerRace === random) {
-        console.log (`Empate de fuerzas`);
+
+function war(playerRace, shadowRaces) {
+    if (playerRace > shadowRace) {
+        result.innerHTML = `Gana la alianza`
+    } else if (playerRace === shadowRaces) {
+        result.innerHTML = `Empate de fuerzas`
     } else {
-        console.log (`Gana la sombra`)
+        result.innerHTML = `Gana la sombra`
     }
 }
+
+function handleClick(event) {
+    event.preventDefault();
+    war();
+}
+
+button.addEventListener('click', handleClick)
